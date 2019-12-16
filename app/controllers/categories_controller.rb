f65ab16480ed27a02fdd87f29ad0051d2d2fa136
sarzynski <kamil.s.sarzynski@gmail.com>
2019-12-17 00:20:40 +0100

    add controllers

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,45 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
+
+    def index
+      @categories = Category.all
+      render json: @categories
+    end
+
+    def show
+      @category = Category.find(params[:id])
+      render json: @category
+    end
+
+    def create
+      @category = Category.new(category_params)
+
+      if @category.save
+        render json: @category
+      else
+        render json: @category.errors
+      end
+    end
+
+    def update
+      if @category.update(category_params)
+        render json: @category
+      else
+        render json: @category
+      end
+    end
+
+    def destroy
+      @category.destroy
+    end
+
+    private
+
+    def set_screening
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:title)
+    end
+end
